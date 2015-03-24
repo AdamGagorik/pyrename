717d7a3f7fc428dfdc7444695b9b2fa3cd7d0fa6
Adam Gagorik <adam.gagorik@gmail.com>
2015-03-24 10:30:01 -0500

    Improve logging

--- pyrename/logutils.py
+++ pyrename/logutils.py
@@ -1,6 +1,38 @@
+import contextlib
+import warnings
 import logging
 
-def setup_logging():
-    lfmt = '[%(asctime)s][%(process)5d][%(levelname)-5s]: %(message)s'
-    dfmt = "%Y-%m-%d %H:%M:%S"
-    logging.basicConfig(level=logging.INFO, format=lfmt, datefmt=dfmt)
+
+def setup_logging(**kwargs):
+    _kwargs = dict(level=logging.INFO, format=lfmt, datefmt=dfmt)
+    _kwargs.update(**kwargs)
+    logging.basicConfig(**_kwargs)
+    logging.addLevelName(logging.CRITICAL, 'FATAL')
+    logging.addLevelName(logging.WARNING, 'WARN')
+
+
+def custom_warning_format(message, category, filename, lineno, *args, **kwargs):
+    return '%s:%s\n%s: %s' % (filename, lineno, category.__name__, message)
+
+
+@contextlib.contextmanager
+def capture(capture_warnings=True, reraise=False):
+    """
+    Log exceptions and warnings.
+    """
+    try:
+        if capture_warnings:
+            default_warning_format = warnings.formatwarning
+            warnings.formatwarning = custom_warning_format
+            logging.captureWarnings(True)
+        try:
+            yield
+        except Exception as e:
+            logging.exception('caught unhandled excetion')
+            if reraise:
+                if not isinstance(e, Warning):
+                    raise
+    finally:
+        if capture_warnings:
+            warnings.formatwarning = default_warning_format
+            logging.captureWarnings(False)
